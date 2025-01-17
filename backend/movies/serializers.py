b913039255caef3c19875fe1d451838d4e1d364f
EuCaue <caueadriano.castro@gmail.com>
2025-01-16 21:34:04 -0300

    feat(backend)!: changed passwords fields name in register
    - Changed `password1` -> `password`
    - Changed `password2` -> `password_confirm`

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -14,6 +14,26 @@ class CustomRegisterSerializer(RegisterSerializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
 
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop("password1", None)
+        self.fields.pop("password2", None)
+
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError(
+                {"password_confirm": "The password should be the same."}
+            )
+        return data
+
+    def save(self, request):
+        self.validated_data["password1"] = self.validated_data.pop("password")
+        self.validated_data["password2"] = self.validated_data.pop("password_confirm")
+        return super().save(request)
+
 
 class CustomUserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
